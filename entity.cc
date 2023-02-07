66208d78a235bade2ba1d947ebad4d98c4c91e96
Gustav <sir.gustav.the.coder@gmail.com>
2023-02-07 23:21:12 +0100

    Add more notes

--- entity.cc
+++ entity.cc
@@ -1,15 +1,22 @@
 // "ecs" strcutrure
 
+struct Guid
+{
+    uint8_t data[16];
+};
+
+/** A generic thing that you can't derive from.
+*/
 struct Entity
 {
-    int guid;
-    Component components[];
+    Guid guid;
+    std::vector<Component> components;
     EntitySystemUpdate systems;
 
     // dynamically add/remove components
 
     bool is_spatial_entity() const { return root != nullptr; }
-    SpatialComponent* root;
+    SpatialComponent* root = nullptr;
 
     void activate()
     {
@@ -24,14 +31,13 @@ struct Entity
 
 enum class EntityState
 {
-    // all components are unloaded
+    /// all components are unloaded
     unloaded,
     
-    // all components are loaded
+    /// all components are loaded
     loaded,
     
-    // entity has been "turned on" in the world
-    // entity components have been registered with all systems
+    /// entity has been "turned on" in the world and entity components have been registered with all systems
     activated
 };
 
@@ -43,43 +49,64 @@ void attach(Entity* parent, Entity* child)
 
 enum class ComponentState
 {
-    // initial state
+    /// initial state
     unloaded,
 
-    // resource load is in progress
+    /// resource load is in progress
     loading,
 
-    // all resources loaded successfully
+    /// all resources loaded successfully
     loaded,
 
-    // some or all resources failed to load
+    /// some or all resources failed to load
     load_failed,
 
-    // component has been initialized (automatically called after loading successfull)
+    /// component has been initialized (automatically called after loading successfull)
     initialized
 };
 
+/** Data storage.
+ * 
+ * Properties that define settings and resource.
+ * 
+ * * Components have no access to other components.
+ * * Components have no access to the entity.
+ * * Components have no default update.
+ * * Inheritance of components is allowed.
+ * * Can contain logic if needed (example: animation graph component contains everything related to the animation graph) and be viewed as a black box.
+*/
 struct ComponentType
 {
-    int id;
-    const char* name;
-    bool max_one_per_entity; // can the enttity have amny components of this type
+    HashedStringView name;
+    
+    /// can the enttity have many components of this type
+    bool max_one_per_entity;
+
+    virtual Component* create() = 0;
 };
 
-struct Component
-{
-    int guid;
-    string name; // debug and tools
+/** Creates a new componet type for built-in components.
+ * 
+ * ```
+ * CUSTOM_COMPONENT(CustomComponent, custom, "custom-name");
+ * ```
+*/
+#define CUSTOM_COMPONENT(TYPE, NAME, HASH)\
+struct TYPE : ComponentType\
+{\
+    constexpr TYPE() : ComponentType{HASH} {}\
+    Component* create() const override;\
+};\
+constexpr TYPE NAME;
 
-    // settings that are serialized
-    // resources that are loaded
-
-    // no access to other compoennts
-    // no acess to the entity
-    // no "default" update
-    // can be inherited
-
-    /*
+/**
+ * * no access to other compoennts
+ * * no acess to the entity
+ * * no "default" update
+ * * can be inherited
+ *
+ * Loading state switching between @ref ComponentState
+    ```graphviz
     digraph G
     {
         unloaded -> loading [label="load()"];
@@ -94,15 +121,38 @@ struct Component
         loaded -> initialized[label="initialize()"];
         initialized -> loaded[label="shutdown()"];
     }
-    */
-    void on_load(); void on_unload();
-    void on_initialize(); void on_shutdown();
+    ```
+*/
+struct Component
+{
+    virtual ~Component() = default;
+
+    Guid guid;
+
+    /// for debug and tools
+    std::string name;
+
+    // settings that are serialized
+    // resources that are loaded
+
+    virtual void on_load(); virtual void on_unload();
+    virtual void on_initialize(); virtual void on_shutdown();
 };
 
 struct SpatialComponent : Component
 {
-    mat4 local_transform;
-    mat4 global_transform; // private set
+private:
+    mat4f _local_transform;
+    mat4f _global_transform;
+
+    void set_global_transform(const mat4f& m) { _global_transform = m; }
+
+public:
+    void set_local_transform(const mat4f& m) { _local_transform = m; local_transform_has_changed(); }
+    
+
+    const mat4f& get_local_transform() { return _local_transform; };
+    const mat4f& get_global_transform() { return _global_transform; };
 
     void local_transform_has_changed()
     {
@@ -110,8 +160,8 @@ struct SpatialComponent : Component
         // update world bounds
         // update world transforms on children
     }
-`
-    private:
+
+private:
     // non-inclusive
     Obb local_bounds;
     Obb world_bounds;
@@ -123,8 +173,8 @@ struct SpatialComponent : Component
 
 struct ComponentFactory
 {
-    void add(ComponentType* name, CreatorFunc);
-    ComponentType* from_name(string name);
+    void add(ComponentType* name);
+    ComponentType* from_name(HashedStringView name);
     Component* create(ComponentType* name);
 };
 
@@ -144,26 +194,41 @@ struct EntitySystemUpdate
     void remove(EntitySystem*, UpdateStage, int prio);
 };
 
-// can't reference other entities
+/** A local system for a entity.
+ * 
+ * only one system of a specified type is allowed per entity
+ * 
+ * * It's manually added to a entity
+ * * It can have entity specific data
+ * * It can't reference other entities
+ * 
+ * Examples
+ *  * animation
+ *  * movement
+ *  * targeting
+ */
 struct EntitySystem
 {
-    // manually added to a entity
-    // can have entity specific data
-
-    // compoennt requirements: requires and optionals for this system to work
+    // compoent requirements: requires and optionals for this system to work
     // tooling: user can see if the system is missing components 
 };
 
-// only one system of a specified ype is allowed per owner (entity/world)
 
-// entity/local systems: animation, movement, targeting
-// world/global systems: skeletal mesh, physics, static mesh, navmesh
-
+/** A global system for the world.
+ * 
+ * Only one system of a specified type is allowed per world
+ * 
+ * Primary role is to handle global world state.
+ * Secondary role is to do data transfer between entitites.
+ * 
+ * Examples:
+ *  * skeletal mesh
+ *  * physics
+ *  * static mesh
+ *  * navmesh
+*/
 struct WorldSystem
 {
-    // primary role: handle global world state
-    // secondary role: data transfer between entitites
-
     // callback for when component is added to the world
 };
 
