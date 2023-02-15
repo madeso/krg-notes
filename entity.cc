d23c3f525ebf348e02dacb1c622b47194da62cc0
Gustav <sir.gustav.the.coder@gmail.com>
2023-02-15 23:49:52 +0100

    Add some more changes, notes and thoughts

--- entity.cc
+++ entity.cc
@@ -42,6 +42,25 @@ struct Entity
     {
         systems.update(stage);
     }
+
+    /// Load all components (resource, memory...)
+    void load();
+
+    /// Unload all components (resource, memory...)
+    void unload();
+
+    /** Turn the enity on in the world.
+     * * register entity for all systems
+     * * create update list
+     * * create entity attachment
+     * 
+     * activate components/local systems paralellized with one enity per thread
+     * activate world systems with one system per thread
+    */ 
+    void activate();
+
+    /// Turn the entity off, remove entity from all systems
+    void deactive();
 };
 
 enum class EntityState
@@ -192,10 +211,14 @@ private:
     // parent spatial components + socket attachment
     Entity* parent;
 
+    /// can reference other spatial components: https://youtu.be/jjEsB611kxs?t=6639
+    /// @todo find out if this can reference entities in other components or not
     std::vector<SpatialComponent*> children;
 
     // only components that can reference other components
     // all components in graph must belong to the same entity (so same thread)
+
+    
 };
 
 /** Helpful factory to create Component.
@@ -280,7 +303,7 @@ With tooling a user can see if the added system is missing components.
 
 Example: Character animation system requires Animation and Skeletal Mesh and has a optional Cloth.
 */
-struct RequiredComponents
+struct RequestedComponents
 {
     std::vector<ComponentType*> required;
     std::vector<ComponentType*> optional;
@@ -301,9 +324,15 @@ struct RequiredComponents
  */
 struct EntitySystem
 {
-    /// get required components
+    /// get requested components
     /// @todo move to a entity sytem type
-    virtual RequiredComponents get_requirements() = 0;
+    virtual RequestedComponents get_component_requests() = 0;
+
+    /*
+    design thought: who removes this from the update list when it's destroyed?
+    better to return a register request and let the caller add/remove stages+prios to the update list?
+    */
+    virtual void register_updates(EntitySystemUpdate* updates) = 0;
 
     /// Called by EntitySystemUpdate
     virtual void update(UpdateStage stage) = 0;
@@ -346,19 +375,29 @@ Cons:
  * Primary role is to handle global world state.
  * Secondary role is to do data transfer between entitites.
  * 
+ * 
  * Examples:
  *  * skeletal mesh
  *  * physics
  *  * static mesh
  *  * navmesh
+ * 
+ * @todo Add types similar to ComponentType and ComponentFactory
 */
 struct WorldSystem
 {
-    /// get required components
-    virtual RequiredComponents get_requirements() = 0;
+    virtual void register_updates(WorldSystemUpdate* updates) = 0;
+
+    /*
+    Design thought: if this is used to transfer data between components,
+    should there be some ui hint system similar to get_component_requests?
+    */
 
     /// Called by WorldSystemUpdate
     virtual void update(UpdateStage stage) = 0;
+
+    virtual void system_was_added_to_world() = 0;
+    virtual void system_was_removed_from_world() = 0;
 
     /// a component was added or activated on a entity
     virtual void component_was_added(Entity* ent, Component* c) = 0;
@@ -383,8 +422,12 @@ struct World
 
     void update(UpdateStage s)
     {
-        entities.update(s); // parallelized, spatial parent is updated before child (worker threads: nuber of cores - 1)
-        systems.update(s); // sequential, can use worker threads if needed
+        // parallelized, spatial parent is updated before child (worker threads: nuber of cores - 1)
+        // place attached entities on the same thread as parent, schedule parent to update before the child
+        entities.update(s);
+
+        // sequential, can use worker threads if needed
+        systems.update(s);
     }
 };
 
