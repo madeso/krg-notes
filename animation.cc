52ecfaf3bf8c84045f466ba54a6fac8e716f50c0
Gustav <sir.gustav.the.coder@gmail.com>
2024-04-15 20:36:19 +0200

    Merge old notes into new doc

--- animation.cc
+++ animation.cc
@@ -1,4 +1,20 @@
+namespace std
+{
+    template <typename T>
+    struct vector{};
+
+    template<typename T> using optional = T*;
+}
+template<typename T> bool is_within(T, T, T);
+struct mat4{};
+struct vec3{};
+struct quat{};
+void assert(bool);
+
+// Animation ((not just sampling and blending)
 // https://www.youtube.com/watch?v=Jkv0pbp0ckQ
+
+// todo: add "generate a pose pool" somewhere
 
 // ===========================================================================
 // SKINNING
@@ -8,8 +24,8 @@
 // multipart character: mesh might not reference all core bones
 
 // deformation helper bones:
-// deformation issues is solved with procedurally generated "deformation helper bones":
-// * roll/twist bones, twist with a percent of the parent
+// deformation issues is solved with procedurally calculated "deformation helper bones":
+// * roll/twist bones, that take a percent of the parent
 // * pokes
 // * look at
 // a topic of its own https://youtu.be/Jkv0pbp0ckQ?t=958
@@ -41,19 +57,31 @@
 struct Mesh
 {
     // mesh data with weights
-    // mesh skeleton with skellington bones
-    // functions to procedurally calculate other bones
+    // mesh skeleton
+	// mapping function that take a skellington and maps to actual bones
+    // and procedurally calculate other bones
+
+// why seperated? reuse anim, support different characters, multipart character/add clothing support
+// clothing can change bone count
+// torso: tank-top 10-15 bones, jacket 25-50, jacket introduce RBF
+// different render/mesh skeletons based on lod?
 };
 
-// animation skeleton
+// gameplay/animation skeleton
+// core bones + gameplay bones (shared asset on disk: female01, male01, horse01)
 struct Skellington
 {
+	// parent data
 };
 
+struct Transform { vec3 translation; quat rotation; vec3 scale; };
 // contain core bones in bone space
-struct Pose
-{
-};
+using Pose = std::vector<Transform> transforms;
+
+
+// sample source animation at some fps, function to extract pose from animation time
+// interpolate between two keyframes
+Pose sample_animation(const Animation&, float t);
 
 // another struct to contain gameplay bones?
 
@@ -65,13 +93,39 @@ struct Pose
 // Render Pose = Pose using render skeleton, generally converted to character space, and then relative to bind pose for skinning
 
 
-// Animation = Set of Poses that produce motion
+// Animation = (roughly) a set of Poses that produce motion
 // generally acceptable to change speed with 20% to match gameplay metrics
 // when imported the animation is sampled (preferable at the same fps that it was authred at)
 // common techniques: curve fitting, quantization+RLE
 // https://www.reddit.com/r/GraphicsProgramming/comments/ffhjqt/an_idiots_guide_to_animation_compression/
 // https://takinginitiative.net/2020/03/07/an-idiots-guide-to-animation-compression/
 // tldr: save animation by track each frame, if position/scale [x, y or z] is the same only serialize a constant
+struct AnimationShared
+{
+	std::vector<std::vector<float>> float_tracks;
+	std::vector<std::vector<quat>> rotation_tracks;
+};
+struct AnimationTrack
+{
+	template<typename T=float>
+	struct Data {bool single; union { T single_value; int index; } data; };
+	std::optional<Data> pos_x;
+	std::optional<Data> pos_y;
+	std::optional<Data> pos_z;
+	std::optional<Data<quat>> rotation;
+	std::optional<Data> scale_x;
+	std::optional<Data> scale_y;
+	std::optional<Data> scale_z;
+	
+	Transform get_transform(const AnimationShared&, int start_index, float scaled_offset);
+};
+
+struct Animation
+{
+	AnimationShared shared;
+	std::vector<AnimationTrack> tracks; // per bone
+};
+
 
 // Sampling = read transform of the keyframes we are between and interpolate
 
@@ -88,13 +142,13 @@ struct Pose
 
 // sometime you might want to blend things globally
 
-struct InterpolativeBlend
+struct Blend_Interpolative
 {
     static quat rotation(quat from, quat to, float t) { return slerp(from, to, t); }
     static vec3 translation(vec3 from, vec3 to, float t) { return lerp(from, to, t); }
     static vec3 scale(vec2 from, vec3 to, float t) { return lerp(from, to, t); }
 };
-struct AdditiveBlend
+struct Blend_Additive
 {
     // operator*
     // Concatenate the rotation of this onto rhs and return the result i.e. first rotate by rhs then by this
@@ -103,12 +157,18 @@ struct AdditiveBlend
     static vec3 translation(vec3 from, vec3 to, float t) { return to*t + from; }
     static vec3 scale(vec2 from, vec3 to, float t) { return to*t + from; }
 };
+
+/// extra factors when blending, useful for "layers"
+struct BoneMask { std::vector<float> mask; };
+
 template<typename TBlend>
 void local_blend(Pose source_pose, Pose target_pose, float blend_weight, BoneMask mask, Pose* result)
 {
+	assert(is_within(0.0f, weight, 1.0f));
+    assert(result != nullptr);
     for(int bone_id: result->bone_count)
     {
-        auto weight = calc_bone_weight(blend_weight, mask, bone_id);
+        const float weight = calc_bone_weight(blend_weight, mask, bone_id);
         if(weight == 0)
         {
             result->transform[bone_id] = source_pose.transform[bone_id];
@@ -130,9 +190,9 @@ void local_blend(Pose source_pose, Pose target_pose, float blend_weight, BoneMas
 // tldr: use vector slerp
 
 // ===========================================================================
-// Bone space: relative to parent bone
+// Bone space: relative to parent bone, "local" in DCC
 // Character space: Relative to character root
-// World space: Relative to scene origin
+// World space: Relative to scene origin, "global" in DCC
 
 // Bone mask: weight per bone in the animation skeleton
 // tool support: define shoulder 100%, hand 20% and feather bones in between
