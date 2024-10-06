003448c7a08d66bb688bdce8828a9aa41c8f7bdb
Gustav <sir.gustav.the.coder@gmail.com>
2024-10-06 13:31:15 +0200

    Add audio notes

---
+++ audio.cc
@@ -0,0 +1,7 @@
+// thin matrix audio system: https://www.youtube.com/watch?v=MEizUamJTyk
+
+// jon blow:
+// part 1: https://www.youtube.com/watch?v=b8zk_z1c914
+// part 2: https://www.youtube.com/watch?v=9xsiWx1xEg8
+// sound hud: https://www.youtube.com/watch?v=6ajukJj5ImI
+// improvements: https://www.youtube.com/watch?v=CTdscl-MH4I
