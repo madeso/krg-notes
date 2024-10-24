d03f605702a3717f0780e01e6785ed8183c02606
Gustav <sir.gustav.the.coder@gmail.com>
2024-10-25 00:19:41 +0200

    Add audio link

--- audio.cc
+++ audio.cc
@@ -5,3 +5,7 @@
 // part 2: https://www.youtube.com/watch?v=9xsiWx1xEg8
 // sound hud: https://www.youtube.com/watch?v=6ajukJj5ImI
 // improvements: https://www.youtube.com/watch?v=CTdscl-MH4I
+
+// frictional games "drag" sound modifcation
+// https://github.com/FrictionalGames/AmnesiaTheDarkDescent/blob/acc95cdedd6c94db89dc924eb9afa23185df562f/HPL2/core/sources/physics/SurfaceData.cpp#L133
+
